09ca5eca45b53053303487184add68c82223ac97
Raj Kumar Meena <rajblogr97@gmail.com>
2026-03-02 15:14:31 +0530

    Update emotion_detection.py

--- EmotionDetection/EmotionDetection/emotion_detection.py
+++ EmotionDetection/EmotionDetection/emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
 
@@ -7,21 +8,36 @@ def emotion_detector(text_to_analyze):
             "error": "Invalid text! Please try again."
         }
 
-    emotions = {
-        "anger": 0.1,
-        "disgust": 0.05,
-        "fear": 0.1,
-        "joy": 0.6,
-        "sadness": 0.15
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
     }
 
-    dominant_emotion = max(emotions, key=emotions.get)
+    response = requests.post(url, json=input_json, headers=header)
 
-    return {
-        "anger": emotions["anger"],
-        "disgust": emotions["disgust"],
-        "fear": emotions["fear"],
-        "joy": emotions["joy"],
-        "sadness": emotions["sadness"],
-        "dominant_emotion": dominant_emotion
-    }
+    if response.status_code == 200:
+
+        formatted_response = json.loads(response.text)
+
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+        dominant_emotion = max(emotions, key=emotions.get)
+
+        return {
+            "anger": emotions["anger"],
+            "disgust": emotions["disgust"],
+            "fear": emotions["fear"],
+            "joy": emotions["joy"],
+            "sadness": emotions["sadness"],
+            "dominant_emotion": dominant_emotion
+        }
+
+    elif response.status_code == 400:
+        return {
+            "error": "Bad request. Status code 400"
+        }
